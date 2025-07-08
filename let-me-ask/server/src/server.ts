5660fbc1ca7453c270d6027966fc8c962fbd26e4
Moisés <moisesnetored@gmail.com>
2025-07-07 23:38:48 -0400

    chore: add biome and ultracite

--- let-me-ask/server/src/server.ts
+++ let-me-ask/server/src/server.ts
@@ -1,27 +1,26 @@
-import { fastify } from "fastify"
-import { 
-  serializerCompiler,
-  validatorCompiler,
-  type ZodTypeProvider
-} from "fastify-type-provider-zod"
-import { fastifyCors } from "@fastify/cors"
-import { env } from "./env.ts"
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
-})
+	origin: "http://localhost:5173",
+});
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.get("/health", () => {
-  return "OK"
-})
+	return "OK";
+});
 
-app.listen({
-  port: env.PORT
-}).then(() => {
-  console.log("HTTP server running")
-})
+app
+	.listen({
+		port: env.PORT,
+	})
