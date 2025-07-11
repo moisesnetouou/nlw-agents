0d3572d65df716a8182b8cc70f60e977df7c71ee
Moisés <moisesnetored@gmail.com>
2025-07-11 00:14:20 -0400

    feat: add create room questions mutation request

--- let-me-ask/web/src/http/use-rooms.ts
+++ let-me-ask/web/src/http/use-rooms.ts
@@ -8,26 +8,7 @@ export function useRooms() {
       const response = await fetch('http://localhost:3333/rooms')
       const result: GetRoomsResponse = await response.json()
 
-      return [
-        {
-          id: '1',
-          name: 'Sala 1',
-          questionsCount: 0,
-          createdAt: new Date().toISOString(),
-        },
-        {
-          id: '2',
-          name: 'Sala 2',
-          questionsCount: 0,
-          createdAt: new Date().toISOString(),
-        },
-        {
-          id: '3',
-          name: 'Sala 3',
-          questionsCount: 5,
-          createdAt: new Date().toISOString(),
-        },
-      ]
+      return result
     },
   })
 }
